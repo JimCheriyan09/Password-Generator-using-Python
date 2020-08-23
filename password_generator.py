79c77b2bbd02effce518bfe9acb25ff2ad513f50
JimCheriyan09 <55669851+JimCheriyan09@users.noreply.github.com>
2020-08-23 16:50:39 +0530

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,40 @@
+import random
+import string
+
+uppercase_letters = string.ascii_uppercase
+lowercase_letters = string.ascii_lowercase
+digits = string.digits
+punctuation = string.punctuation
+password_list = {1: uppercase_letters,
+                 2: lowercase_letters,
+                 3: digits,
+                 4: punctuation}
+
+while True:
+    try:
+        password_length = int(
+            input("Enter the length of the password you want to generate: "))
+        print('\n')
+        if password_length <= 0:
+            print("The Password length s negative or null as password length")
+        elif password_length > 0:
+            break
+    except ValueError:
+        print("Please enter a proper integer for your password length ")
+
+print("\n".join(["MENU", "1: Uppercase Letters",
+                 "2: Lowercase Letters", "3: Numerics", "4: Punctuations"]))
+
+choice = (input("Enter you choice (For Ex: 124) : "))
+choice_list = list(choice)
+choice_list = list(map(int, choice_list))
+
+
+new_character_list = []
+for i in choice_list:
+    new_character_list.extend(password_list.get(i))
+
+random.shuffle(new_character_list)
+
+print("Your password is: ", end=" ")
+print("".join(new_character_list[0:password_length]))
